f151b3c5905e1b77e86767f1f88f1c6018683ef4
hicham erragragui <hicham.erragragui@yassir.com>
2022-08-17 15:43:29 +0100

    omnidb storage

--- tf-files/filestore.tf
+++ tf-files/filestore.tf
@@ -13,3 +13,20 @@ resource "google_filestore_instance" "filestore-hicham-iac" {
     modes   = ["MODE_IPV4"]
   }
 }
+
+
+resource "google_filestore_instance" "filestore-hicham-iac-omnidb" {
+  name = "filestore-hicham-iac-omnidb"
+  location = "europe-west2-a"
+  tier = "STANDARD"
+
+  file_shares {
+    capacity_gb = 1024
+    name        = "share2"
+  }
+
+  networks {
+    network = google_compute_network.vpc-hicham-iac.name
+    modes   = ["MODE_IPV4"]
+  }
+}
